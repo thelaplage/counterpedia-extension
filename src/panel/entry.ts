31a5cc49c1b0a9506c64bf684c12779871c71c8c
thelaplage <19560430+thelaplage@users.noreply.github.com>
2026-08-16 01:02:57 -0400

    feat(check): initialize researcher teaching surface

--- src/panel/entry.ts
+++ src/panel/entry.ts
@@ -4,9 +4,11 @@ import { initCheckAnatomy } from "./checkAnatomy";
 import { initInquiryPaths } from "./inquiryPaths";
 import { initInquiryTrace } from "./inquiryTrace";
 import { initResearcherProfiles } from "./researcherProfiles";
+import { initResearcherTeaching } from "./researcherTeaching";
 
 initDirectionalActions();
 initCheckAnatomy();
 initInquiryPaths();
 initInquiryTrace();
 initResearcherProfiles();
+initResearcherTeaching();
